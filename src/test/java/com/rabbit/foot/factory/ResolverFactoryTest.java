4dbd39e3dcde702d530f08a347ee8adf6e2ae9c3
liaoas <liao991224@gmail.com>
2024-12-10 10:55:53 +0800

    引入junit5

---
+++ src/test/java/com/rabbit/foot/factory/ResolverFactoryTest.java
@@ -0,0 +1,13 @@
+package com.rabbit.foot.factory;
+
+import org.junit.jupiter.api.Test;
+
+class ResolverFactoryTest {
+
+
+    @Test
+    void search() {
+        System.out.println("search");
+    }
+
+}
